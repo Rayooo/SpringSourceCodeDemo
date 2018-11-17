2eb2bbd16cd9d1d68872081964ebae3e2e8948e5
Ray <chenxiaolei.ray@gmail.com>
2018-11-17 11:32:00 +0800

    初始化

---
+++ src/main/java/com/ray/springsourcecodedemo/SpringSourceCodeDemoApplication.java
@@ -0,0 +1,12 @@
+package com.ray.springsourcecodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSourceCodeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSourceCodeDemoApplication.class, args);
+    }
+}
